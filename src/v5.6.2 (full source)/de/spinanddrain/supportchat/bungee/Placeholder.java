434a8d9d006c31dc053a30a2de5f9d191e673f58
SpinAndDrain <47568186+SpinAndDrain@users.noreply.github.com>
2020-03-11 20:16:15 +0100

    Create Placeholder.java

---
+++ src/v5.6.2 (full source)/de/spinanddrain/supportchat/bungee/Placeholder.java
@@ -0,0 +1,17 @@
+package de.spinanddrain.supportchat.bungee;
+
+public class Placeholder {
+
+	/*
+	 * Created by SpinAndDrain on 19.12.2019
+	 */
+
+	protected String holder;
+	protected String placer;
+	
+	public Placeholder(String holder, String placer) {
+		this.holder = holder;
+		this.placer = placer;
+	}
+	
+}
