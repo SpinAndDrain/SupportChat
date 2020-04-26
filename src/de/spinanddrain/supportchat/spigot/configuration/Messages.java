752d7754815fef912fc6e6c3981773059abf8e2f
SpinAndDrain <47568186+SpinAndDrain@users.noreply.github.com>
2020-04-26 12:52:37 +0200

    Updated to SupportChat v6.1

--- src/de/spinanddrain/supportchat/spigot/configuration/Messages.java
+++ src/de/spinanddrain/supportchat/spigot/configuration/Messages.java
@@ -10,21 +10,22 @@ public enum Messages {
 	
 	PREFIX("&7[&6SupportChat&7]", "&7[&6SupportChat&7]"),
 	NO_PERMISSION("&cKeine Rechte!", "&cNo permission!"),
-	CANT_EXECUTE_COMMAND("&cDu kannst diesen Befehl nicht ausführen!", "&cYou can't execute this command!"),
-	NEW_REQUEST("&9Eine neue Supportanfrage ist verfübar! &7(/requests)", "&9A new request is available! &7(/requests)"),
+	CANT_EXECUTE_COMMAND("&cDu kannst diesen Befehl nicht ausfÃ¼hren!", "&cYou can't execute this command!"),
+	NEW_REQUEST("&9Eine neue Supportanfrage ist verfÃ¼bar! &7(/requests)", "&9A new request is available! &7(/requests)"),
 	QUEUE_JOINED("&aDu befindest dich nun in der Warteschlange! Bitte habe etwas Geduld!", "&aYou are in the queue now. Please have some patience."),
 	ALREADY_IN_QUEUE("&cDu befindest dich bereits in der Warteschlange!", "&cYou are already in the queue!"),
 	CRWL("&cDu kannst keinen Support anfordern, wenn du selbst ein Supporter bist!", "&cYou can't request support while you are logged in."),
 	CLWR("&cDu kannst dich nicht einloggen, wenn du Support angefordert hast!", "&cYou can't log in if you have requested support!"),
-	CONVERSATION_ALREADY_RUNNING("&cDie Konversation läuft bereits!", "&cThe conversation is already running!"),
+	CONVERSATION_ALREADY_RUNNING("&cDie Konversation lÃ¤uft bereits!", "&cThe conversation is already running!"),
 	CONVERSATION_NOT_STARTED("&cDie Konversation wurde noch nicht gestartet!", "&cThe conversation was not started yet!"),
-	REQUEST_NO_LONGER_AVAILABE("&cDie Anfrage ist nicht mehr verfügbar!", "&cThe request is no longer available!"),
+	REQUEST_NO_LONGER_AVAILABE("&cDie Anfrage ist nicht mehr verfÃ¼gbar!", "&cThe request is no longer available!"),
+	REQUESTS_AVAILABLE("&b[count] &9requests are still available", "&b[count] &9Anfragen sind noch unbearbeitet"),
 	YOU_ARE_NOW_IN_A_CONVERSATION("&6Du befindest dich nun in einer Konversation mit &b[player]&6!", "&6You are now in a conversation with &b[player]&6!"),
 	SUCCESSFULLY_DENIED("&eErfolgreich abgelehnt.", "&eSuccessfully denied."),
 	YOU_GOT_DENIED("&cDeine Anfrage wurde abgelehnt!", "&cYour request was denied!"),
 	NO_CONVERSATION("&cDu befindest dich in keiner Konversation!", "&cYou are not in a conversation!"),
 	ALREADY_IN_CONVERSATION("&cDu befindest dich bereits in einer Konversation!", "&cYou are already in a conversation!"),
-	ONLY_LEADERS("&cNur der Konversationsleiter darf das Gespräch beenden!", "&cOnly the conversation leader is allowed to end a conversation!"),
+	ONLY_LEADERS("&cNur der Konversationsleiter darf das GesprÃ¤ch beenden!", "&cOnly the conversation leader is allowed to end a conversation!"),
 	CONVERSATION_ENDED("&eDie Konversation wurde beendet!", "&eThe conversation was ended!"),
 	ALREADY_LOGGED_IN("&cDu bist bereits eingeloggt!", "&cYou are already logged in!"),
 	NOT_LOGGED_IN("&cDu bist nicht eingeloggt!", "&cYou are not logged in!"),
@@ -36,8 +37,8 @@ public enum Messages {
 	NOBODY_ONLINE("&eDerzeit ist niemand online.", "&eCurrently nobody online."),
 	LISTEN_JOIN("&7[&a+&7] &7[player]", "&7[&a+&7] &7[player]"),
 	LISTEN_LEAVE("&7[&c-&7] &7[player]", "&7[&c-&7] &7[player]"),
-	INVALID_CONVERSATION_ID("&cUngültige ID!", "&cInvalid ID!"),
-	SUCCESSFULLY_JOINED("&eErfolgreich der Konversation §7([id]) §ebeigetreten!", "&eSuccessfully joined the conversation &7([id])&e!"),
+	INVALID_CONVERSATION_ID("&cUngÃ¼ltige ID!", "&cInvalid ID!"),
+	SUCCESSFULLY_JOINED("&eErfolgreich der Konversation Â§7([id]) Â§ebeigetreten!", "&eSuccessfully joined the conversation &7([id])&e!"),
 	SUCCESSFULLY_LEAVED("&cDu hast die Konversation verlassen.", "&cConversation leaved."),
 	RELOADING("&eReloading...", "&eReloading..."),
 	SUCCESSFULLY_RELOADED("&aReloaded!", "&aReloaded!"),
@@ -53,13 +54,13 @@ public enum Messages {
 	INVENTORY_ID("&eConversation-ID: [id]", "&eConversation-ID: [id]"),
 	MANAGER_REQUEST("&9&lAnfrag verwalten", "&9&lManage request"),
 	INVENTORY_ACCEPT("&aAnnehmen", "&aAccept"),
-	INVENTORY_BACK("&cZurück", "&cBack"),
-	INVENTORY_LISTEN("&eZuhören", "&eListen"),
+	INVENTORY_BACK("&cZurÃ¼ck", "&cBack"),
+	INVENTORY_LISTEN("&eZuhÃ¶ren", "&eListen"),
 	INVENTORY_DENY("&cAblehnen", "&cDeny"),
 	
 	SYNTAX("&cSyntax: &e[command]", "&cSyntax: &e[command]"),
 	SYNTAX_SUPPORT("/support [<Grund>]", "/support [<reason>]"),
-	SYNTAX_REASONS("&fVerfügbare Gründe: &a[reasons]", "&fAvailable reasons: &a[reasons]"),
+	SYNTAX_REASONS("&fVerfÃ¼gbare GrÃ¼nde: &a[reasons]", "&fAvailable reasons: &a[reasons]"),
 	SYNTAX_REQUESTS("/requests oder /req", "/requests or /req"),
 	SYNTAX_END("/scend", "/scend"),
 	SYNTAX_LOGIN("/sclogin [<hidden>]", "/sclogin [<hidden>]"),
@@ -90,7 +91,7 @@ public enum Messages {
 	}
 	
 	public String getWithoutPrefix() {
-		return handler.configuration.getString(path).replaceAll("&", "§");
+		return handler.configuration.getString(path).replaceAll("&", "Â§");
 	}
 	
 	public String getMessage() {
@@ -102,7 +103,7 @@ public enum Messages {
 	}
 	
 	public String getWithoutPrefixWithPlaceholder(Placeholder... placeholders) {
-		String message = handler.configuration.getString(path).replaceAll("&", "§");
+		String message = handler.configuration.getString(path).replaceAll("&", "Â§");
 		for(Placeholder i : placeholders) {
 			message = message.replace(i.placeholder, i.replacement);
 		}
